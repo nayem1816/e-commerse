2015a9c224d69d3754daadb132be4b526fcb5d2e
Md Nayem Hossain <nayemhossainmd1@gmail.com>
2022-09-28 09:51:25 +0600

    trending offer added

--- scripts/swiper.js
+++ scripts/swiper.js
@@ -25,6 +25,14 @@ var trendingSwiper = new Swiper('.trendingSwiper', {
             slidesPerView: 4,
             spaceBetween: 40,
         },
+        1440: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+        },
+        1600: {
+            slidesPerView: 5,
+            spaceBetween: 50,
+        },
     },
 });
 
@@ -47,6 +55,41 @@ var dealsSwiper = new Swiper('.dealsSwiper', {
         1280: {
             slidesPerView: 4,
             spaceBetween: 40,
+        },
+    },
+});
+
+var swiper = new Swiper('.mySwiper', {
+    slidesPerView: 'auto',
+    centeredSlides: true,
+    spaceBetween: 10,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    initialSlide: 1,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+        768: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+        1024: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        1280: {
+            slidesPerView: 2,
+            spaceBetween: 30,
         },
     },
 });
